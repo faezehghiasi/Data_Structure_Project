555006868a9fe7d54d2794ad19b932d054106b19
fatemehprb <fatemepoorbagheri1382@yahoo.com>
2024-01-22 23:50:38 +0330

    Neighbourhood class was added

---
+++ Pizza_finder/Pizza_finder/Neighbourhood.cpp
@@ -0,0 +1,7 @@
+#include "Neighbourhood.h"
+Neighbourhood::Neighbourhood(Point p1Val, Point p2Val, Point p3Val, Point p4Val) {
+	this->p1 = p1Val;
+	this->p2 = p2Val;
+	this->p3 = p3Val;
+	this->p4 = p4Val;
+}
--- Pizza_finder/Pizza_finder/Point.h
+++ Pizza_finder/Pizza_finder/Point.h
@@ -1,8 +1,11 @@
 #pragma once
+#include<iostream>
 class Point {
 	double x;
 	double y;
 public:
+	Point(){}
 	Point(double xVal, double yVal) :x(xVal), y(yVal) {}
 	friend class BasicNode;
+	Point& operator =(const Point& p);
 };
