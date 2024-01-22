da52f695a32937d7ba739231808b4f19ed36ce79
fatemehprb <fatemepoorbagheri1382@yahoo.com>
2024-01-23 00:17:58 +0330

    Main branch class was added

--- Pizza_finder/Pizza_finder/Node_MainPizza.cpp
+++ Pizza_finder/Pizza_finder/Node_MainPizza.cpp
@@ -1,1 +1,4 @@
 #include "Node_MainPizza.h"
+void Node_MainPizza::addBranch(Node_SubPizza input) {
+	this->branches.push_back(input);
+}
--- Pizza_finder/Pizza_finder/Node_MainPizza.h
+++ Pizza_finder/Pizza_finder/Node_MainPizza.h
@@ -1,9 +1,15 @@
 #pragma once
-#include"BasicNode.h"
+#include"Node_SubPizza.h"
 #include<iostream>
 #include<vector>
 using namespace std;
 class Node_MainPizza:public BasicNode
 {
+private:
+	vector< Node_SubPizza> branches;
+public:
+	Node_MainPizza(double xVal, double yVal, string nameVal, string mainBranchNameVal)
+		: BasicNode(xVal, yVal, nameVal){}
+	void addBranch(Node_SubPizza input);
 };
 
