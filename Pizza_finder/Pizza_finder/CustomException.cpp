cf63288d12492caa52f71a471d99b2ad3c6c856f
faezehghiasi <f.ghiasi@ec.iut.ir>
2024-12-12 22:05:46 +0330

    Remove unnecessary files

--- Pizza_finder/Pizza_finder/CustomException.cpp
+++
@@ -1,4 +0,0 @@
-#include "CustomException.h"
-const char* CustomException::what()const noexcept {
-    return errorMessage;
-}
--- Pizza_finder/Pizza_finder/Node_SubPizza.cpp
+++
@@ -1,6 +0,0 @@
-#include "Node_SubPizza.h"
-void Node_SubPizza::print() {
-	cout << name << " " << "location : " << "( " << coordinates.x << ", " << coordinates.y << " )" << endl;
-}
-//***************************************************************************************************
-string Node_SubPizza::getMainBranchName() { return mainBranchName; }
